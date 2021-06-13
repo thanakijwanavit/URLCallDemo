11aad908eb3592a33292e3fbf92232dcac5e1d04
nic <nwanavit@hatari.cc>
2021-06-13 14:21:08 +0700

    Initial Commit

---
+++ Shared/URLCallDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLCallDemoApp.swift
+//  Shared
+//
+//  Created by nic wanavit on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct URLCallDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
